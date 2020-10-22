d1cc4980496b2721f9d9f15603aed24a51da2751
mindaugas.staselis <mindaugas.staselis@gmail.com>
2020-10-22 19:47:11 +0300

    use css purge only when using prod env

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,4 +1,4 @@
-import { build } from 'aurelia-cli';
+import { build, CLIOptions } from 'aurelia-cli';
 import * as gulp from 'gulp';
 import * as project from '../aurelia.json';
 import * as postcss from 'gulp-postcss';
@@ -7,13 +7,16 @@ import * as cssnano from 'cssnano';
 import * as postcssUrl from 'postcss-url';
 import * as tailwindcss from 'tailwindcss';
 
-export default function processCSS() { 
+export default function processCSS() {
+  const useCssProcessing = CLIOptions.getEnvironment() === 'prod';
+  const cssProcessPipe = postcss([
+    autoprefixer(),
+    postcssUrl({ url: 'inline', encodeType: 'base64' }),
+    cssnano(),
+    tailwindcss({ config: useCssProcessing ? 'tailwind.prod.config.js' : 'tailwind.config.js' })
+  ]);
+
   return gulp.src(project.cssProcessor.source, { sourcemaps: true, since: gulp.lastRun(processCSS) })
-    .pipe(postcss([
-      autoprefixer(),
-      postcssUrl({ url: 'inline', encodeType: 'base64' }),
-      cssnano(),
-      tailwindcss({ config: 'tailwind.config.js' })
-    ]))
+    .pipe(cssProcessPipe)
     .pipe(build.bundle());
 }
